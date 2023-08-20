77e6c95ceb057e2b6657590e6fc3c40b91f9976d
LunarExalt <142700679+LunarExalt@users.noreply.github.com>
2023-08-20 01:30:31 +0000

    initialcommithere

---
+++ MAIN.js
@@ -0,0 +1,15 @@
+console.log("Hello world")
+console.log("How are you this fine day?")
+x=1
+if(x==2){
+    console.log("This is true.")
+} else {
+    console.log("This ain't true foo'!")
+}
+for(let i=0; i<9; i+=2){
+    console.log("I equals "+i)
+}
+function sayHello(){
+    console.log("Hello")
+}
+sayHello()
